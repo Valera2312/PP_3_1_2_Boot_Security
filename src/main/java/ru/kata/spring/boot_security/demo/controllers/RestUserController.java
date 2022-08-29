db731c86d15fe399a04425d713c5e551a543541a
Valera2312 <33982431+Valera2312@users.noreply.github.com>
2022-08-29 12:47:04 +1000

    ok

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/RestUserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/RestUserController.java
@@ -1,52 +1,52 @@
-package ru.kata.spring.boot_security.demo.controllers;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.service.RoleService;
-import ru.kata.spring.boot_security.demo.service.UserService;
-
-import java.security.Principal;
-import java.util.Arrays;
-import java.util.List;
-
-@RestController
-public class RestUserController {
-
-
-    private final UserService userService;
-
-    private final RoleService roleService;
-
-    @Autowired
-    public RestUserController(UserService userService,RoleService roleService) {
-        this.roleService = roleService;
-        this.userService = userService;
-    }
-
-
-    @RequestMapping(value = {"showUsers"}, method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody List<User> showAllUsers() {
-        return userService.listUsers();
-    }
-    @RequestMapping(value = {"showCurrentUser"}, method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody User showCurrentUser(Principal principal) {
-        return userService.findByLogin(principal.getName());
-    }
-    @GetMapping(value = {"admin/delete/{id}"},produces = "application/json")
-    public @ResponseBody void deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-    }
-    @PostMapping(path = "admin/add",produces = "application/json" )
-
-    public @ResponseBody void addUser(@ModelAttribute("user") User user,
-                          @RequestParam(name = "roleCheckbox",defaultValue = "false")String checkboxValue) {
-        String[] checkBoxArr = checkboxValue.split("ROLE_");
-        for (String roleName:
-                checkBoxArr) {
-            user.addRole(roleService.findByName("ROLE_" + roleName));
-        }
-        userService.addUser(user);
-    }
-
-}
+//package ru.kata.spring.boot_security.demo.controllers;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//import ru.kata.spring.boot_security.demo.models.User;
+//import ru.kata.spring.boot_security.demo.service.RoleService;
+//import ru.kata.spring.boot_security.demo.service.UserService;
+//
+//import java.security.Principal;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@RestController
+//public class RestUserController {
+//
+//
+//    private final UserService userService;
+//
+//    private final RoleService roleService;
+//
+//    @Autowired
+//    public RestUserController(UserService userService,RoleService roleService) {
+//        this.roleService = roleService;
+//        this.userService = userService;
+//    }
+//
+//
+//    @RequestMapping(value = {"showUsers"}, method = RequestMethod.GET, produces = "application/json")
+//    public @ResponseBody List<User> showAllUsers() {
+//        return userService.listUsers();
+//    }
+//    @RequestMapping(value = {"showCurrentUser"}, method = RequestMethod.GET, produces = "application/json")
+//    public @ResponseBody User showCurrentUser(Principal principal) {
+//        return userService.findByLogin(principal.getName());
+//    }
+//    @GetMapping(value = {"admin/delete/{id}"},produces = "application/json")
+//    public @ResponseBody void deleteUser(@PathVariable Long id) {
+//        userService.deleteUser(id);
+//    }
+//    @PostMapping(path = "admin/add",produces = "application/json" )
+//
+//    public @ResponseBody void addUser(@ModelAttribute("user") User user,
+//                          @RequestParam(name = "roleCheckbox",defaultValue = "false")String checkboxValue) {
+//        String[] checkBoxArr = checkboxValue.split("ROLE_");
+//        for (String roleName:
+//                checkBoxArr) {
+//            user.addRole(roleService.findByName("ROLE_" + roleName));
+//        }
+//        userService.addUser(user);
+//    }
+//
+//}
